6c54e99b94c44e80084978b57b6985e3b383eb2c
Daniel Granhão <Daniel@silicon.fe.up.pt>
2019-03-26 17:48:56 +0000

    removed basic folder

--- prototype/basic/setup_sim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-python /usr/local/share/opae/ase/scripts/ipc_clean.py
-afu_sim_setup -f -s hw/rtl/sources.txt -p intg_xeon -t VCS build_sim
-cd build_sim
-sed -i '237s/UNKNOWN/arria10/' Makefile
-make
-make sim
