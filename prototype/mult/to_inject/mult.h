7234366bbb5863ff2ced1d89946a2bf75a4b9cb7
Daniel Granhão <Daniel@silicon.fe.up.pt>
2019-03-26 17:46:52 +0000

    trying to delete files

--- prototype/mult/to_inject/mult.h
+++
@@ -1,6 +0,0 @@
-#ifndef mult_h__
-#define mult_h__
-
-extern uint64_t mult(uint64_t a, uint64_t b);
-
-#endif // mult_h__
--- prototype/mult/to_inject/test_lib/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <inttypes.h>
-
-#include "../mult.h"
-
-int main(){
-	printf("5*10 = %d\n", mult(5,10));
-}
